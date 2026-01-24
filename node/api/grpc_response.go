80c77d18ca7fee68cbe6a5d7f3c43b6236b739b9
Adam <cortez24rus@gmail.com>
2026-01-24 21:30:39 +0700

    26.1.24.1

--- node/api/grpc_response.go
+++ node/api/grpc_response.go
@@ -27,7 +27,7 @@ type ApiResponse struct {
 
 // GetApiResponse retrieves statistics from the gRPC server for Xray or Singbox.
 func GetApiResponse(cfg *config.NodeConfig) (*ApiResponse, error) {
-	grpcTarget := fmt.Sprintf("%s:%s", cfg.Core.ApiGrpcAddress, cfg.Core.ApiGrpcPort)
+	grpcTarget := fmt.Sprintf("%s:%d", cfg.Core.ApiGrpcAddress, cfg.Core.ApiGrpcPort)
 	cfg.Logger.Debug("Connecting to gRPC server XRAY/SINGBOX", "address", grpcTarget)
 
 	clientConn, err := grpc.NewClient(grpcTarget, grpc.WithTransportCredentials(insecure.NewCredentials()))
