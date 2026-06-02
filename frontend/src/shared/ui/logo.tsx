73610c4dc1f1c758342520d17b7c3e67fe4990c7
Adam <cortez24rus@gmail.com>
2026-06-02 21:36:50 +0700

    26.6.2

--- frontend/src/shared/ui/logo.tsx
+++ frontend/src/shared/ui/logo.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable @stylistic/indent */
 import { Box, BoxProps, ElementProps } from '@mantine/core'
 interface LogoProps
-    extends ElementProps<'svg', keyof BoxProps>,
-        Omit<BoxProps, 'children' | 'ref'> {
+    extends ElementProps<'svg', keyof BoxProps>, Omit<BoxProps, 'children' | 'ref'> {
     size?: number | string
 }
 export function Logo({ size, style, ...props }: LogoProps) {
