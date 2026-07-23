7ee2f6e3aee20fd81c417d55fe84d1f40e982e4b
Adam <cortez24rus@gmail.com>
2026-07-24 02:00:24 +0700

    fix(frontend): remove .app-config-v2.json expectation, use app-config.json

--- frontend/vendor/@exodus/subscription-page-types/build/backend/constants/app-config.constant.d.ts
+++ frontend/vendor/@exodus/subscription-page-types/build/backend/constants/app-config.constant.d.ts
@@ -1,3 +1,3 @@
-export declare const APP_CONFIG_ROUTE_LEADING_PATH: "/assets/.app-config-v2.json";
-export declare const APP_CONFIG_ROUTE_WO_LEADING_PATH: "assets/.app-config-v2.json";
+export declare const APP_CONFIG_ROUTE_LEADING_PATH: "/assets/app-config.json";
+export declare const APP_CONFIG_ROUTE_WO_LEADING_PATH: "assets/app-config.json";
 //# sourceMappingURL=app-config.constant.d.ts.map
