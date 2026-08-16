c0542b48be19e4b7a86bd751eb09cc5c11ef0889
Adam <cortez24rus@gmail.com>
2026-08-16 10:50:53 +0700

    fix(auth): log 4xx unauthorized errors at debug level instead of error

--- backend/internal/httpapi/shared/error.go
+++ backend/internal/httpapi/shared/error.go
@@ -7,8 +7,12 @@ import (
 )
 
 func SendError(w http.ResponseWriter, code int, msg string, err error, cfg *config.BackendConfig) {
-	if err != nil && cfg != nil {
-		cfg.Logger.Error(msg, "error", err)
+	if err != nil && cfg != nil && cfg.Logger != nil {
+		if code >= 500 {
+			cfg.Logger.Error(msg, "error", err)
+		} else {
+			cfg.Logger.Debug(msg, "error", err)
+		}
 	}
 	WriteJSONError(w, code, msg)
 }
